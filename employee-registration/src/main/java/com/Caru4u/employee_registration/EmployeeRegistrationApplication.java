f8a4470a552672987b8efd2c37d84f878f453893
Caru4u <caru4uadmin@gmail.com>
2026-04-15 11:57:09 +0530

    Add files via upload

---
+++ employee-registration/src/main/java/com/Caru4u/employee_registration/EmployeeRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.Caru4u.employee_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistrationApplication.class, args);
+	}
+
+}
