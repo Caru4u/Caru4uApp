5eb99ef04f9463a235f2fd713bde3c05622eb34f
Caru4u <caru4uadmin@gmail.com>
2026-09-12 18:45:48 +0530

    Add files via upload

---
+++ Caru4u-Products/src/main/java/com/Caru4u/Caru4u_Products/Caru4uProductsApplication.java
@@ -0,0 +1,13 @@
+package com.Caru4u.Caru4u_Products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Caru4uProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Caru4uProductsApplication.class, args);
+	}
+
+}
