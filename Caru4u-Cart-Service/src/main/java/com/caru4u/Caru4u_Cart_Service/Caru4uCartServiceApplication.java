d28357de5d2635c4d80027c91f63cceb62590fd1
Caru4u <caru4uadmin@gmail.com>
2026-09-14 23:11:20 +0530

    Add files via upload

---
+++ Caru4u-Cart-Service/src/main/java/com/caru4u/Caru4u_Cart_Service/Caru4uCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.caru4u.Caru4u_Cart_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Caru4uCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Caru4uCartServiceApplication.class, args);
+	}
+
+}
---
+++ Caru4u-Cart-Service/src/test/java/com/caru4u/Caru4u_Cart_Service/Caru4uCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caru4u.Caru4u_Cart_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Caru4uCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
